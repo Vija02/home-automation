31dbb1caa3cec45c7572a52651f0966ba417fbc1
Michael Salim <salim_michaelmi@yahoo.com>
2018-11-28 20:54:19 +0000

    feat: use relative url, add toast notif and sync data with polling

--- heater/frontend/src/App.js
+++ heater/frontend/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
 import Slider from 'react-rangeslider'
+import { ToastContainer, toast } from 'react-toastify'
 import axios from 'axios'
 
 import thermometer from './thermometer.svg'
 
 import './App.css'
 
-const base_url = 'http://raspberrypi:3000'
+// Maybe make this setable in the future
+const PORT = 3000
+const base_url = `${window.location.protocol}//${window.location.hostname}:${PORT}`
 
 class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = { value: 500 }
+
+		this.checkCurrentTemp = this.checkCurrentTemp.bind(this)
 	}
 	componentDidMount() {
+		this.checkCurrentTemp()
+		setInterval(() => {
+			this.checkCurrentTemp()
+		}, 10000);
+	}
+
+	checkCurrentTemp() {
 		axios
 			.get(`${base_url}/status`)
 			.then(res => {
@@ -24,6 +36,7 @@ class App extends Component {
 				console.log('No last recorded')
 			})
 	}
+
 	render() {
 		const verticalLabels = {
 			500: '* - OFF',
@@ -36,6 +49,17 @@ class App extends Component {
 		}
 		return (
 			<div className="App">
+				<ToastContainer
+					position="bottom-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop
+					closeOnClick
+					rtl={false}
+					pauseOnVisibilityChange
+					draggable
+					pauseOnHover
+				/>
 				<h1>
 					<img src={thermometer} width={30} alt="thermometer" />
 					Heater
@@ -50,7 +74,10 @@ class App extends Component {
 						format={val => Math.floor(((val - 500) / 2000) * 100) + '%'}
 						onChange={value => {
 							this.setState({ value })
-							axios.get(`${base_url}/${value}`)
+							axios.get(`${base_url}/${value}`).catch(() => {
+								toast.error('Error: Failed to set heater', { toastId: 'errorset' })
+								this.checkCurrentTemp()
+							})
 						}}
 						className="container-height"
 					/>
